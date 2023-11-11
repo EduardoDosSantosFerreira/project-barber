1b45152e69886bb8b42569d318cc4a62310ac7a4
Eduardo Dos Santos Ferreira <139907452+EduardoDosSantosFerreira@users.noreply.github.com>
2023-11-11 17:34:52 -0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$('.hero-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: 'linear',
+});
